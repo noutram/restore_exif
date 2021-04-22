0b0c502d31cf38c4ff41ef5c60157ddcaf6eba50
Nicholas Outram <nicholas@outram.org>
2021-04-22 09:59:07 +0100

    now logs

--- restore_exif.cpp
+++ restore_exif.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include "rapidjson/document.h"
+#include <chrono>
+#include <ctime> 
 
 using namespace std;
 using namespace rapidjson;
@@ -10,14 +12,22 @@ using namespace rapidjson;
 // Credit to https://rapidjson.org 
 
 void printChildren(rapidjson::Value& obj);
-
+string preferredTag(Document& d);
 //#define VERBOSE
 
 int main(int argc, char* argv[])
 {
+    //UTC
+    time_t now = time(0);
+    tm* gmtm = gmtime(&now);
+    char *dt = asctime(gmtm);
+
+    ofstream log("restore_exif.log",ofstream::out | ofstream::app);
     #ifdef VERBOSE
     cout << "restore_exif " << argc << " parameters\n";
     #endif
+    cout << "Job Started " << dt;
+    log << "Job started " << gmtm->tm_hour << ":" << gmtm->tm_min << ":" << gmtm->tm_sec << " " << gmtm->tm_mday << "/" << gmtm->tm_mon << "/" << gmtm->tm_year << ",";
 
     //Base filename
     string fn;
@@ -39,6 +49,7 @@ int main(int argc, char* argv[])
     #ifdef VERBOSE
     cout << "Processing " << fn << " using " << fnjpg << endl;
     #endif
+    log << "," << fn << "," << fnjpg;
 
     //Open JSON File and parse
     string jsonString;
@@ -46,10 +57,12 @@ int main(int argc, char* argv[])
     ip.open(fnjpg);
     if (!ip) {
         cout << "Cannot find corresponding JSON file " << fnjpg << endl;
+        log << ",No JSON file found" << endl;
+        log.close();
         return -1;
     }
 
-    //TBD - Read JSON file
+    //Read JSON file
     string ln;
     while (getline(ip, ln)) {
         // Append the text from the file
@@ -67,8 +80,11 @@ int main(int argc, char* argv[])
     doc.Parse(jsonString.c_str());
 
     //Find best timestamp
-    string tag;
-    if (!doc.HasMember(tag.c_str())) {
+    string tag = preferredTag(doc);
+    if (tag == "") {
+        cout << "No suitable JSON tag for " << fnjpg << endl;
+        log << ",No JSON timestamp was found" <<  endl;
+        log.close();
         return 0; 
     }
 
@@ -78,16 +94,22 @@ int main(int argc, char* argv[])
     string timeFormatted = v["formatted"].GetString();
     cout << timeFormatted << endl;
     cout << "@" << timeStampString << endl;
-    
+    log << "," << timeStampString << "," << timeFormatted; 
+        
     //Perform timestamp fix
     string cmd = "touch --date=@" + timeStampString + " " + fn;
     cout << cmd << endl;
-    system(cmd.c_str());
-
+    int res = system(cmd.c_str());
+    if (res != 0) {
+        log << ",Failed to touch file" << endl;
+    } else {
+        log << ",OK" << endl;
+    }
     #ifdef VERBOSE
     printChildren(v);
     #endif   
 
+    log.close();
     return 0;
 }
 
@@ -99,3 +121,15 @@ void printChildren(rapidjson::Value& obj)
         printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
     }
 }
+
+string preferredTag(Document& d)
+{
+    const string preferred[] = {"photoTakenTime", "creationTime"};
+    for (string s : preferred) {
+        if (d.HasMember(s.c_str())) {
+            return s;
+        }
+    }
+    return "";
+}
+
